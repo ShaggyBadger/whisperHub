0bdde15d9a5d557036f381a446fc298053d638c3
Josh <josh.ballard1983@gmail.com>
2026-01-01 08:11:56 -0500

    adding trusted middlewere

--- main/server/app/__init__.py
+++ main/server/app/__init__.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    TrustedHostMiddleware,
+    allowed_hosts=["*"]
+)
 
 # Import endpoints to register them
 from . import main
